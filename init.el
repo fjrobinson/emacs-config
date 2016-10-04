b9834f0a1b05c40270ed20dbbe22d64170cda4e1
Jamie Robinson <fjrobinson@ku.edu>
2016-10-04 14:28:52 -0500

    Fixed keybinding for ace-jump-mode to work with M-x shell

--- init.el
+++ init.el
@@ -132,7 +132,7 @@
 (defun enable-ace-jump-mode ()
   "Enable ace jump mode."
   (require 'ace-jump-mode)
-  (define-key global-map (kbd "C-c SPC") 'ace-jump-mode))
+  (define-key global-map (kbd "C-c C-SPC") 'ace-jump-mode))
 
 (defun enable-multiple-cursors ()
   (require 'multiple-cursors)
